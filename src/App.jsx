45b1acef19f6c23a705d3edb1d9f46735070de87
Adelkilde <William.adelkilde@gmail.com>
2023-09-29 12:39:14 +0200

    member.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import reactLogo from "./assets/react.svg";
+import Member from "./components/Member";
 
 function App() {
-    return (
-        <>
-            <header>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-                <h1>RACE Your React 🎉</h1>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1>RACE Your React 🎉</h1>
+      </header>
+      <Member member={{ firstName: "Peter 🧙‍♂️" }} />
+      <Member member={{ firstName: "Race 💙" }} />
+      <Member member={{ firstName: "Edith 🥧" }} />
+      <Member member={{ firstName: "Oskar 🥤" }} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Member.jsx
@@ -0,0 +1,8 @@
+function Member({ member }) {
+  return (
+    <article>
+      <h2>{member.firstName}</h2>
+    </article>
+  );
+}
+export default Member;
